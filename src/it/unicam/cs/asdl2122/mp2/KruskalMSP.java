703d420119fd628a8f3766d4f0d201865f851565
Federico Maria Cruciani <federico.cruciani@live.com>
2021-12-23 19:16:52 +0100

    Initial commit

---
+++ src/it/unicam/cs/asdl2122/mp2/KruskalMSP.java
@@ -0,0 +1,55 @@
+package it.unicam.cs.asdl2122.mp2;
+
+import java.util.Set;
+
+//ATTENZIONE: è vietato includere import a pacchetti che non siano della Java SE
+
+/**
+ * Classe singoletto che implementa l'algoritmo di Kruskal per trovare un
+ * Minimum Spanning Tree di un grafo non orientato, pesato e con pesi non
+ * negativi. L'algoritmo implementato si avvale della classe
+ * {@code ForestDisjointSets<GraphNode<L>>} per gestire una collezione di
+ * insiemi disgiunti di nodi del grafo.
+ * 
+ * @author Luca Tesei (template)
+ * Federico Maria Cruciani, federicomar.cruciani@studenti.unicam.it (implementazione)
+ * 
+ * @param <L> tipo delle etichette dei nodi del grafo
+ */
+public class KruskalMSP<L> {
+
+    /**
+     * Struttura dati per rappresentare gli insiemi disgiunti utilizzata
+     * dall'algoritmo di Kruskal.
+     */
+    private ForestDisjointSets<GraphNode<L>> disjointSets;
+
+    // TODO inserire eventuali variabili istanza o classi interne necessarie
+
+    /**
+     * Costruisce un calcolatore di un albero di copertura minimo che usa
+     * l'algoritmo di Kruskal su un grafo non orientato e pesato.
+     */
+    public KruskalMSP() {
+        this.disjointSets = new ForestDisjointSets<GraphNode<L>>();
+    }
+
+    /**
+     * Utilizza l'algoritmo goloso di Kruskal per trovare un albero di copertura
+     * minimo in un grafo non orientato e pesato, con pesi degli archi non
+     * negativi. L'albero restituito non è radicato, quindi è rappresentato
+     * semplicemente con un sottoinsieme degli archi del grafo.
+     * 
+     * @param g un grafo non orientato, pesato, con pesi non negativi
+     * @return l'insieme degli archi del grafo g che costituiscono l'albero di
+     *         copertura minimo trovato
+     *
+     * @throws NullPointerException se il grafo g è null
+     * @throws IllegalArgumentException se il grafo g è orientato, non pesato o
+     * con pesi negativi
+     */
+    public Set<GraphEdge<L>> computeMSP(Graph<L> g) {
+        // TODO implementare
+        return null;
+    }
+}
