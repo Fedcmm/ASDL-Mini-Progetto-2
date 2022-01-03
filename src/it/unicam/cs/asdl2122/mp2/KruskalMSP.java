1a92acfbfc738bd795c3210315ddae572ad385f8
Federico Maria Cruciani <federico.cruciani@live.com>
2022-01-03 20:12:49 +0100

    Finished KruskalMSP

--- src/it/unicam/cs/asdl2122/mp2/KruskalMSP.java
+++ src/it/unicam/cs/asdl2122/mp2/KruskalMSP.java
@@ -1,5 +1,8 @@
 package it.unicam.cs.asdl2122.mp2;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 //ATTENZIONE: è vietato includere import a pacchetti che non siano della Java SE
@@ -16,6 +19,7 @@ import java.util.Set;
  * 
  * @param <L> tipo delle etichette dei nodi del grafo
  */
+@SuppressWarnings({"ConstantConditions", "FieldMayBeFinal"})
 public class KruskalMSP<L> {
 
     /**
@@ -24,14 +28,12 @@ public class KruskalMSP<L> {
      */
     private ForestDisjointSets<GraphNode<L>> disjointSets;
 
-    // TODO inserire eventuali variabili istanza o classi interne necessarie
-
     /**
      * Costruisce un calcolatore di un albero di copertura minimo che usa
      * l'algoritmo di Kruskal su un grafo non orientato e pesato.
      */
     public KruskalMSP() {
-        this.disjointSets = new ForestDisjointSets<GraphNode<L>>();
+        this.disjointSets = new ForestDisjointSets<>();
     }
 
     /**
@@ -49,7 +51,64 @@ public class KruskalMSP<L> {
      * con pesi negativi
      */
     public Set<GraphEdge<L>> computeMSP(Graph<L> g) {
-        // TODO implementare
-        return null;
+        if (g == null)
+            throw new NullPointerException();
+        if (g.isDirected())
+            throw new IllegalArgumentException();
+
+        Set<GraphEdge<L>> edges = g.getEdges();
+        for (GraphEdge<L> edge : edges) {
+            if (!edge.hasWeight() || edge.getWeight() < 0)
+                throw new IllegalArgumentException();
+        }
+
+        Set<GraphEdge<L>> result = new HashSet<>();
+
+        disjointSets.clear();
+        for (GraphNode<L> node : g.getNodes()) {
+            disjointSets.makeSet(node);
+        }
+
+        List<GraphEdge<L>> edgeList = new ArrayList<>(g.getEdges());
+        sortEdges(edgeList, 0, edgeList.size() - 1);
+
+        for (GraphEdge<L> edge : edgeList) {
+            GraphNode<L> node1 = edge.getNode1();
+            GraphNode<L> node2 = edge.getNode2();
+            if (!disjointSets.findSet(node1).equals(disjointSets.findSet(node2))) {
+                result.add(edge);
+                disjointSets.union(node1, node2);
+            }
+        }
+        
+        return result;
+    }
+
+    private void sortEdges(List<GraphEdge<L>> edges, int low, int high) {
+        if (low < high) {
+            int pi = partition(edges, low, high);
+
+            sortEdges(edges, low, pi - 1);
+            sortEdges(edges, pi + 1, high);
+        }
+    }
+
+    private int partition(List<GraphEdge<L>> edges, int low, int high) {
+        GraphEdge<L> pivot = edges.get(high);
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            GraphEdge<L> jEdge = edges.get(j);
+            if (jEdge.getWeight() <= pivot.getWeight()) {
+                i++;
+
+                GraphEdge<L> iEdge = edges.set(i, jEdge);
+                edges.set(j, iEdge);
+            }
+        }
+
+        GraphEdge<L> iEdge = edges.set(i + 1, pivot);
+        edges.set(high, iEdge);
+        return i + 1;
     }
 }
